8e26379bb270d4551523a14d1b0471e1d4068e58
Lia-MC <lia.mc73700@gmail.com>
2026-02-02 12:53:09 -0500

    noted outline for general game logic and potential ui tools

---
+++ outlines.py
@@ -0,0 +1,17 @@
+# LOGIC
+
+# generate the equation 
+# prompt user + start timer
+# get user input 
+# check user input validity (is it a valid equation) 
+# check user input correctness (does it match the generated equation)
+# feedback sound if progress made
+# repeat until fully correct equation or until 10 minutes have passed
+# stop timer 
+# display time taken + accuracy via number of tries taken
+# log results into file
+
+
+# TOOLS 
+# streamlit 
+# html
